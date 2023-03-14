c5124641565b16c37f772676d525dff4dee947d3
Preslav Getov <preslav.getov@yara.com>
2023-03-14 10:48:08 +0200

    basic form now works on blur and onsubmit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,28 +1,81 @@
 import React from "react";
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
 const FormStyles = styled.form`
-  padding: 1rem;
+  background-color: #f4f4f4;
+  padding: 20px;
+  border-radius: 5px;
+  margin-top: 1rem;
 `;
+const StyledLabel = styled.label`
+  font-weight: bold;
+`;
+const StyledInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-top: 0.5rem;
+  outline: none;
+`;
+
+const StyledButton = styled.button`
+  background-color: #4caf50;
+  color: white;
+  padding: 10px;
+  margin-top: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  &:disabled {
+    opacity: 0.5;
+  }
+  &:enabled {
+    opacity: 1;
+  }
+  opacity: ${(props) => (!props.enabled ? 0.5 : 1)};
+`;
+
+const StyledAlert = styled.div`
+  padding: 10px;
+  background-color: #f44336;
+  color: white;
+  margin-top: 10px;
+  border-radius: 5px;
+`;
+
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ mode: "all" });
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <FormStyles>
-      <fieldset>
-        <legend>Request maintenance quote</legend>
-        <p>
-          <label for="land_size_input">
-            Land size
-            <input type="text" name="size" id="land_size_input" value="small" />
-          </label>
-        </p>
-        <p>
-          <input type="radio" name="size" id="size_2" value="medium" />
-          <label for="size_2">Medium</label>
-        </p>
-        <p>
-          <input type="radio" name="size" id="size_3" value="large" />
-          <label for="size_3">Large</label>
-        </p>
-      </fieldset>
+    <FormStyles onSubmit={handleSubmit(onSubmit)}>
+      <legend>Get a Farm Maintenance Quote</legend>
+      <div>
+        <StyledLabel for="fullName">Full Name</StyledLabel>
+        <StyledInput defaultValue="test" {...register("full-name")} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <StyledLabel for="areaCode">Area Code</StyledLabel>
+        <StyledInput
+          {...register(
+            "areaCode",
+            { required: true },
+            { onBlur: handleSubmit(onSubmit) }
+          )}
+        />
+
+        <StyledButton type="submit">Submit</StyledButton>
+      </div>
+
+      <div>
+        {errors.areaCode && <StyledAlert>This field is required</StyledAlert>}
+      </div>
     </FormStyles>
   );
 };
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 const MainStyles = styled.main`
   display: flex;
+  gap: 10rem;
 `;
 const MainSection = ({ children }) => {
   return <MainStyles>{children}</MainStyles>;
