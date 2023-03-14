915f780fadfea31dd425ee1e0b1c9159f1528476
Preslav Getov <preslav.getov@yara.com>
2023-03-14 13:56:45 +0200

    form done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -57,41 +57,68 @@ const Form = () => {
               message: "Please make sure form is filled",
             },
             pattern: {
-              value: /[^0-9]{1,}/,
+              value: /^([^0-9]*)$/,
               message: "Please enter only word chars or special symbols",
             },
           })}
         />
 
-        {/* <StyledLabel htmlFor="sea">Sea pressure</StyledLabel>
+        <StyledLabel htmlFor="area">Area in square meters</StyledLabel>
         <StyledInput
-          {...register(
-            "sea",
-            { required: true },
-            { onBlur: handleSubmit(onSubmit) }
-          )}
+          {...register("areaSqr", {
+            required: {
+              value: true,
+              message: "Please make sure form is filled",
+            },
+            pattern: {
+              value: /^([1-9]*)$/,
+              message: "Please enter only nums between 1-9",
+            },
+          })}
         />
-        <div>
-          {errors.sea && <StyledAlert>This field is required</StyledAlert>}
-        </div>
 
-        <StyledLabel htmlFor="landSqr">Land in square meters</StyledLabel>
+        <StyledLabel htmlFor="phone">Phone Number</StyledLabel>
         <StyledInput
-          {...register(
-            "landSqr",
-            { required: true },
-            { onBlur: handleSubmit(onSubmit) }
-          )}
-        /> */}
+          {...register("phone", {
+            required: {
+              value: true,
+              message: "Please make sure form is filled",
+            },
+            pattern: {
+              value: /^([0-9|\+]*)$/,
+              message:
+                "Please enter a valid phone number. No spaces or dashes.",
+            },
+          })}
+        />
 
-        <div>
-          {errors.fullName?.message && (
-            <StyledAlert>{errors.fullName?.message}</StyledAlert>
-          )}
-        </div>
+        <StyledLabel htmlFor="email">Email</StyledLabel>
+        <StyledInput
+          {...register("email", {
+            required: {
+              value: true,
+              message: "Please make sure form is filled",
+            },
+          })}
+        />
 
         <StyledButton type="submit" title="Submit" />
         <StyledButton title="Cancel" />
+
+        <div>
+          {(errors.fullName?.message && (
+            <StyledAlert>{errors.fullName?.message}</StyledAlert>
+          )) ||
+            (errors.areaSqr?.message && (
+              <StyledAlert>{errors.areaSqr?.message}</StyledAlert>
+            )) ||
+            (errors.phone?.message && (
+              <StyledAlert>{errors.phone?.message}</StyledAlert>
+            )) ||
+            (errors.email?.message && (
+              <StyledAlert>{errors.email?.message}</StyledAlert>
+            ))}
+        </div>
       </div>
     </FormStyles>
   );
